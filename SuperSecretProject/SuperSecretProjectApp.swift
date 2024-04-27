a2a40b72fd378e1dcdafff6d9ba28eb87ada7011
Dennis Vermeulen <d.vermeulen@me.com>
2024-04-27 11:12:15 +0200

    Initial Commit

---
+++ SuperSecretProject/SuperSecretProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSecretProjectApp.swift
+//  SuperSecretProject
+//
+//  Created by Dennis Vermeulen on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperSecretProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
